964d685e56187c95a99d16504d8e518cebcd5bcb
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-05-30 20:55:01 +0530

    Updated RAG for cache

--- appcolab/Chat-Interface.py
+++ appcolab/Chat-Interface.py
@@ -26,9 +26,9 @@ chat_system_prompt = """
 """
 from langchain.llms import HuggingFacePipeline
 try:
-  from langchain_community.document_loaders import JSONLoader
+  from langchain_community.document_loaders import JSONLoader, TextLoader
 except:
-  from langchain_community.document_loaders import PyPDFLoader
+  from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import Chroma
@@ -65,7 +65,93 @@ else:
     dotenv.load_dotenv()
     import os
     access_token_read = os.getenv('HUGGINGFACE_TOKEN')
-    from apis.rag_api import chat
+    #from apis.rag_api import chat
+    @st.cache_resource
+    def llm():
+        # Loading Model and Tokenizer
+        model_checkpoint = 'meta-llama/Meta-Llama-3-8B-Instruct'
+        model_config = AutoConfig.from_pretrained(model_checkpoint, trust_remote_code=True, max_new_tokens=1024, use_auth_token=True)
+
+        model = AutoModelForCausalLM.from_pretrained(model_checkpoint, trust_remote_code=True, config=model_config, device_map='auto', use_auth_token=True)
+
+        tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_auth_token=True)
+
+        # Pipeline Creation
+        pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, torch_dtype=torch.float16, max_length=3000, device_map="auto")
+
+        llmpipe = HuggingFacePipeline(pipeline=pipeline)
+        return llmpipe
+    # Parsing function
+    def parse(string):
+            return string.split("<|end_header_id|>")[-1]
+
+    
+    # Retriever
+    @st.cache_resource
+    def retriever():
+        try:
+            loader = JSONLoader("/data/data.json")
+        except:
+            loader = TextLoader("/data/data.txt")
+        documents = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
+
+        embedding_function = SentenceTransformerEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        vectorstore = Chroma(collection_name="sample_collection", embedding_function = embedding_function)
+
+        vectorstore.add_documents(texts)
+
+        retrieverdata = vectorstore.as_retriever(k=7)
+        return retrieverdata
+    
+    # Pipeline Object
+    @st.cache_resource
+    class Pipeline:
+        def __init__(self,llm,retriever):
+            self.llm = llm
+            self.retriever = retriever
+        def retrieve(self,question):
+            docs = self.retriever.invoke(question)
+            return "\n\n".join([d.page_content for d in docs])
+        # You are a helpful, respectful and honest Sales assistant for Samsung that is made to give answers regarding offers for Samsung Products.
+        #            If the user asks for offers on any phone, give the offers in a tabular format with columns: Offer Provider, Offer Type, Offer Amount, Minimum Purchase Value, Offer Description, Final Price after offer (calculated accordingly).
+        #            The response should be given in Markdown format.
+        #            If the user tries to ask out of topic questions do not engange in the conversation.
+        #            If the given context is not sufficient to answer the question,Do not answer the question.
+        def augment(self,question,context):
+            return f"""
+                <|begin_of_text|>
+                <|start_header_id|>
+                system
+                <|end_header_id|>
+                {chat_system_prompt}
+                <|eot_id|>
+                <|start_header_id|>
+                user
+                <|end_header_id|>
+                Answer the user question based on the context provided below
+                Context :{context}
+                Question: {question}
+                <|eot_id|>
+                <|start_header_id|>
+                assistant
+                <|end_header_id|>"""
+        def parse(self,string):
+            return string.split("<|end_header_id|>")[-1]
+        def generate(self,question):
+            context = self.retrieve(question)
+            prompt  = self.augment(question,context)
+            answer  = self.llm.invoke(prompt)
+            return self.parse(answer)
+    @st.cache_resource    
+    def pipe():    
+        piped = Pipeline(llm(),retriever())
+        return piped
+    def chat(prompt):
+        if(prompt is not None):
+            output = pipe().generate(prompt)
+            return output
     def on_input_change():
         user_input = st.session_state.user_input
         print(repr(user_input))
